c5285b24ed8f7db90e8b4ce64a9d073ea192aa14
DubheGiliandor <julian.b.w.m.hansmann@googlemail.com>
2023-11-22 13:47:36 +0100

    Commit 4: Prototyp Division

--- app/src/main/java/hsw/gradle/template/Division.java
+++ app/src/main/java/hsw/gradle/template/Division.java
@@ -1,5 +1,32 @@
 package hsw.gradle.template;
 
-public class Division
-{
+public class Division{
+    public int divide(int a, int b) throws Exception {
+        if (b != 0) {
+            return  a / b;
+        } else {
+            throw new Exception();
+        }
+    }
+    public double divide(int a, double b) throws Exception {
+        if (b != 0) {
+            return  a / b;
+        } else {
+            throw new Exception();
+        }
+    }
+    public double divide(double a, int b) throws Exception {
+        if (b != 0) {
+            return  a / b;
+        } else {
+            throw new Exception();
+        }
+    }
+    public double divide(double a, double b) throws Exception {
+        if (b != 0) {
+            return  a / b;
+        } else {
+            throw new Exception();
+        }
+    }
 }
