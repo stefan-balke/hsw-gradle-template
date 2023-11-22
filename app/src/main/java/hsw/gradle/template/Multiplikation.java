f5de73bfd62d38d19c8eeacbad8528be359efb80
DubheGiliandor <julian.b.w.m.hansmann@googlemail.com>
2023-11-22 13:47:14 +0100

    Commit 3: Prototyp Multiplikation

--- app/src/main/java/hsw/gradle/template/Multiplikation.java
+++ app/src/main/java/hsw/gradle/template/Multiplikation.java
@@ -1,4 +1,16 @@
 package hsw.gradle.template;
 
 public class Multiplikation {
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+    public double multiply(int a, double b) {
+        return a * b;
+    }
+    public double multiply(double a, int b) {
+        return a * b;
+    }
+    public double multiply(double a, double b) {
+        return a * b;
+    }
 }
