de40017a196d2ca62be1a29b51a001eb0f9c53c5
DubheGiliandor <julian.b.w.m.hansmann@googlemail.com>
2023-11-22 13:46:46 +0100

    Commit 2: Prototyp Subtraktion

--- app/src/main/java/hsw/gradle/template/Subtraktion.java
+++ app/src/main/java/hsw/gradle/template/Subtraktion.java
@@ -1,4 +1,16 @@
 package hsw.gradle.template;
 
 public class Subtraktion {
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public double subtract(int a, double b) {
+        return a - b;
+    }
+    public double subtract(double a, int b) {
+        return a - b;
+    }
+    public double subtract(double a, double b) {
+        return a - b;
+    }
 }
