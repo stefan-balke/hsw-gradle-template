6e2fcfd574438e72fdc6a19a076f038343a452eb
DubheGiliandor <julian.b.w.m.hansmann@googlemail.com>
2023-11-22 13:46:19 +0100

    Commit 1: Prototyp Addition

--- app/src/main/java/hsw/gradle/template/Addition.java
+++ app/src/main/java/hsw/gradle/template/Addition.java
@@ -1,4 +1,16 @@
 package hsw.gradle.template;
 
 public class Addition {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public double add(int a, double b) {
+        return a + b;
+    }
+    public double add(double a, int b) {
+        return a + b;
+    }
+    public double add(double a, double b) {
+        return a + b;
+    }
 }
